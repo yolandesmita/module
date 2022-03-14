23e88b484b0337a35bb238b455037fda7765c071
yolandesmita <101077620+yolandesmita@users.noreply.github.com>
2022-03-14 08:16:04 +0700

    update

--- code.go
+++ code.go
@@ -1,9 +1,31 @@
 package module
 
+// reutrn value
 func SayHello() string {
 	return "Selamat Datang!"
 }
 
+// multiple return value
 func NamaLengkap() (string, string, string) {
 	return "Agus", "Bambang", "Joko"
 }
+
+//interface struct
+
+type OperasiHitung interface {
+	Perkalian() int
+	// Keliling() int
+	// Luas() int
+}
+
+type Nilai struct {
+	a, b, c int
+}
+
+func (n Nilai) Perkalian() int {
+	return n.a * n.b * n.c
+}
+
+// func (in InputPersegi) Luas() int {
+// 	return in.a * in.b
+// }
